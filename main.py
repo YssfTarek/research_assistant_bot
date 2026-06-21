794c3b350df5f3a91b418c41831aa7a90c59a8e6
GenLabs-Bioinfo <ngs@generations-labs.com>
2026-06-21 16:03:50 +0300

    Add supabase test endpoint

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from dotenv import load_dotenv
 import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 from pydantic import BaseModel
+from supabase import create_client, Client
+from langchain_core.tools import tool
 
 load_dotenv()
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+SUPA_API_KEY = os.getenv("SUPA_API_KEY")
+SUPA_URL = os.getenv("SUPA_URL")
 
 model = ChatGoogleGenerativeAI(
     model = "gemini-2.5-flash",
@@ -32,5 +36,39 @@ async def chat_with_google_genai(prompt: Prompt):
     messages = [
         ("human", f"{prompt.query}"),
     ]
+    response = model.invoke(messages)@tool
+def get_supa_rows():
+    supabase: Client = create_client(SUPA_URL, SUPA_API_KEY)
+    response = (
+        supabase.table("exec_assitant_test")
+        .select("*")
+        .execute()
+    )
+    return response.data
+
+
+
+@app.post("/exec_assistant")
+async def exec_assistant(prompt: Prompt):
+    supabase: Client = create_client(SUPA_URL, SUPA_API_KEY)
+    response = (
+        supabase.table("exec_assitant_test")
+        .select("*")
+        .execute()
+    )
+    messages = [
+        ("human", f"{prompt.query}"),
+    ]
     response = model.invoke(messages)
     return response.content
+    return response.content
+
+@app.post("/test_supa")
+async def test_supa():
+    supabase: Client = create_client(SUPA_URL, SUPA_API_KEY)
+    response = (
+        supabase.table("exec_assitant_test")
+        .select("*")
+        .execute()
+    )
+    return response.data
